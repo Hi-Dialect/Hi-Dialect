34fa2323a4fb9f70ae1612a7b639fe59846b2a07
freshwlnd <985605915@qq.com>
2020-03-21 23:06:00 +0800

    创建Activity查询接口*5

--- hidialect_ws/src/main/java/com/hidialect/hidialect_ws/controller/ActivitiesController.java
+++ hidialect_ws/src/main/java/com/hidialect/hidialect_ws/controller/ActivitiesController.java
@@ -1,13 +1,42 @@
 package com.hidialect.hidialect_ws.controller;
 
+import com.hidialect.hidialect_ws.entity.Activities;
 import com.hidialect.hidialect_ws.service.IActivitiesService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/activities")
+@RequestMapping(value = "/activity")
 public class ActivitiesController {
     @Autowired
     private IActivitiesService iActivitiesService;
+    /* 日期：20200321
+     * 创建人：徐悦皓 */
+
+    @RequestMapping(value = "/checkAct",method = RequestMethod.POST)
+    private Activities[] checkAct(@RequestParam int userNo){
+        //return iActivitiesService.checkAct(userNo);
+    }
+
+    @RequestMapping(value = "/getActByTypeTime",method = RequestMethod.POST)
+    private Activities[] getActByTypeTime(@RequestParam String ActType, @RequestParam String startTime){
+        //return iActivitiesService.getActByTypeTime(ActType, startTime);
+    }
+
+    @RequestMapping(value = "/getActivities",method = RequestMethod.GET)
+    private Activities[] getActivities(){
+        //return iActivitiesService.getActivities();
+    }
+
+    @RequestMapping(value = "/getActivitymsgByActId",method = RequestMethod.POST)
+    private Activities getActivitymsgByActId(@RequestParam int actId){
+        //return iActivitiesService.getActivitymsgByActId(actId);
+    }
+
+    @RequestMapping(value = "/getActNaByVdoId",method = RequestMethod.POST)
+    private Activities getActNaByVdoId(@RequestParam int vdoId){
+        //return iActivitiesService.getActNaByVdoId(vdoId);
+    }
+
+
 }
