4be14e9b39e969164b77855c2169067f739f81d9
freshwlnd <985605915@qq.com>
2020-03-21 23:09:01 +0800

    创建Comment查询接口*1

--- hidialect_ws/src/main/java/com/hidialect/hidialect_ws/controller/CommentsController.java
+++ hidialect_ws/src/main/java/com/hidialect/hidialect_ws/controller/CommentsController.java
@@ -1,13 +1,24 @@
 package com.hidialect.hidialect_ws.controller;
 
+import com.hidialect.hidialect_ws.entity.Comments;
 import com.hidialect.hidialect_ws.service.ICommentsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/comments")
+@RequestMapping(value = "/comment")
 public class CommentsController {
     @Autowired
     private ICommentsService iCommentsService;
+    /* 日期：20200321
+     * 创建人：徐悦皓 */
+
+    @RequestMapping(value = "/viewComment",method = RequestMethod.POST)
+    private Comments[] viewComment(@RequestParam int vdoId){
+        //return iCommentsService.viewComment(vdoId);
+    }
+
 }
