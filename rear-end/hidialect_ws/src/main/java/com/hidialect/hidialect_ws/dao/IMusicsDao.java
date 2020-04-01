5f45fc86c58b65459609e4a63896e3a050f7eb1f
wodilaoge <2645189252@qq.com>
2020-04-01 17:36:59 +0800

    新增接口

--- rear-end/hidialect_ws/src/main/java/com/hidialect/hidialect_ws/dao/IMusicsDao.java
+++ rear-end/hidialect_ws/src/main/java/com/hidialect/hidialect_ws/dao/IMusicsDao.java
@@ -1,7 +1,12 @@
 package com.hidialect.hidialect_ws.dao;
 
+import com.hidialect.hidialect_ws.entity.Musics;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IMusicsDao {
+    Musics[] getByVdoId(@Param("vdoId")int vdoId);
+    Musics[] getByMscId(@Param("mscId")int mscId);
+    Musics[] searchMusicByName(@Param("mscNa")String mscNa);
 }
--- rear-end/hidialect_ws/src/main/java/com/hidialect/hidialect_ws/service/IMusicsService.java
+++ rear-end/hidialect_ws/src/main/java/com/hidialect/hidialect_ws/service/IMusicsService.java
@@ -1,4 +1,9 @@
 package com.hidialect.hidialect_ws.service;
 
+import com.hidialect.hidialect_ws.entity.Musics;
+
 public interface IMusicsService {
+    Musics[] getByVdoId(int vdoId);
+    Musics[] getByMscId(int mscId);
+    Musics[] searchMusicByName(String mscNa);
 }
